323637b1533bcecb463645654c57700c106e58b8
honnef <contact@honnef.dev>
2023-02-26 13:59:34 +0100

    Setting up CMakePresets.json

--- cmake/Doctest.cmake
+++ cmake/Doctest.cmake
@@ -6,9 +6,17 @@ if(${enable_tests})
   FetchContent_Declare(
     DocTest
     GIT_REPOSITORY "https://github.com/onqtam/doctest"
+    GIT_SHALLOW TRUE
     GIT_TAG "v2.4.8"
+    # EXCLUDE_FROM_ALL TRUE # doesn't work. check
+    # https://gitlab.kitware.com/cmake/cmake/-/issues/20167
   )
+  # FetchContent_MakeAvailable(DocTest)
 
-  FetchContent_MakeAvailable(DocTest)
-  include_directories(${DOCTEST_INCLUDE_DIR})
+  if(NOT doctest_POPULATED)
+    message(STATUS "Cloning DocTest")
+    FetchContent_Populate(DocTest)
+    add_subdirectory(${doctest_SOURCE_DIR} ${doctest_BINARY_DIR}
+                     EXCLUDE_FROM_ALL)
+  endif()
 endif()
--- cmake/FetchExternal.cmake
+++ cmake/FetchExternal.cmake
@@ -12,9 +12,16 @@ FetchContent_Declare(
   fmtlib
   GIT_REPOSITORY "https://github.com/fmtlib/fmt"
   GIT_TAG "8.0.1"
-  GIT_SHALLOW TRUE)
+  GIT_SHALLOW TRUE
+  # EXCLUDE_FROM_ALL TRUE # doesn't work. check
+  # https://gitlab.kitware.com/cmake/cmake/-/issues/20167
+)
 set(FMT_INSTALL ON)
-# --------------------------------------------------------------------------------
-FetchContent_MakeAvailable(fmtlib)
+# FetchContent_MakeAvailable(fmtlib)
+if(NOT fmtlib_POPULATED)
+  message(STATUS "Cloning fmtlib")
+  FetchContent_Populate(fmtlib)
+  add_subdirectory(${fmtlib_SOURCE_DIR} ${fmtlib_BINARY_DIR} EXCLUDE_FROM_ALL)
+endif()
 # --------------------------------------------------------------------------------
 list(APPEND lst_external fmt::fmt)
